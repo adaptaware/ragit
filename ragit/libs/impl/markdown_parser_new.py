d2db4dcef38d68ea448bf0bf1ede61b00f126753
adaptaware <dev.adaptaware@gmail.com>
2025-01-05 01:04:10 -0500

    wip.

---
+++ ragit/libs/impl/markdown_parser_new.py
@@ -0,0 +1,81 @@
+"""Exposes the functionality to parse a markdown file."""
+
+import abc
+import enum
+
+
+def node_factory(line):
+    """Returns the node type for the passed in line.
+
+    :param str line: The line of text to get its node type.
+
+    :return: The node type and the value of the line.
+    :rtype: Tuple
+    """
+    stripped = line.strip()
+    if stripped.startswith("# "):
+        return H1Node(stripped[2:])
+    elif stripped.startswith("## "):
+        return H2Node(stripped[3:])
+    elif stripped.startswith("### "):
+        return H3Node(stripped[4:])
+    assert False, f"Invalid line: {line}"
+
+
+class Node:
+    def __init__(self, caption):
+        assert caption, "Header caption cannot be empty."
+        self._children = []
+        self._tail = self
+        self._parent = None
+        self._caption = caption
+
+    def _reset_tail(self):
+        self._tail = self
+
+    def __repr__(self):
+        return self._caption
+
+    def to_str(self):
+        lines = self.to_lines()
+        return '\n'.join(lines)
+
+    def to_lines(self, depth=0):
+        lines = []
+        lines.append('---- ' * depth + self._caption)
+        for c in self._children:
+            for l in c.to_lines(depth + 1):
+                lines.append(l)
+        return lines
+
+    def add(self, line):
+        new_node = node_factory(line)
+        self._add_node(new_node)
+
+    def _add_node(self, other):
+        if self._tail.can_add(other):
+            self._tail._children.append(other)
+            other._parent = self._tail
+            self._tail = other
+        else:
+            parent = self._tail._parent
+            parent._reset_tail()
+            parent._add_node(other)
+
+    def can_add(self, other):
+        return isinstance(other, (H1Node, H2Node, H3Node))
+
+
+class H1Node(Node):
+    def can_add(self, other):
+        return isinstance(other, (H2Node, H3Node))
+
+
+class H2Node(Node):
+    def can_add(self, other):
+        return isinstance(other, H3Node)
+
+
+class H3Node(Node):
+    def can_add(self, other):
+        return False
---
+++ ragit/libs/impl/tests/test_markdown_parser_new.py
@@ -0,0 +1,76 @@
+"""Tests the markdown_parser module."""
+
+import os
+
+import pytest
+
+import ragit.libs.impl.markdown_parser_new as mp
+
+_CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def test_document_invalid_caption():
+    with pytest.raises(AssertionError):
+        doc = mp.Node("")
+
+def test_node_1():
+    caption = "root"
+    doc = mp.Node(caption)
+    retrieved = doc.to_str()
+    assert retrieved == caption
+
+def test_node_2():
+    captions = [
+        "# Header 1",
+        "# Header 2",
+    ]
+    root = mp.Node("root")
+
+    for c in captions:
+        root.add(c)
+
+    retrieved = root.to_str()
+    print("******")
+    print(retrieved)
+
+def test_node_3():
+    captions = [
+        "# # Header 1",
+        "## ## Header 2",
+        "### ### Header 3",
+        "## ## Header 4",
+        "### ### Header 5",
+    ]
+    root = mp.Node("root")
+
+    for c in captions:
+        root.add(c)
+
+    retrieved = root.to_str()
+    print("******")
+    print(retrieved)
+
+def test_document2():
+    txt = """
+    # Header1
+    this is a test
+    multi-line
+    ## Header 1.2
+    Belongs to header 1.2..
+    ### Header 1.3
+    Belongs to header 1.3..
+    |name|age|
+    |x|1|
+    # Header2
+    Belongs to header 2
+    # Header3
+    # Header4
+    |name|age|
+    |x|1|
+    """
+    root = markdown_parser.Root()
+    for line in txt.split("\n"):
+        root.add_line(line)
+
+    for n in root.get_nodes():
+        print(n)
